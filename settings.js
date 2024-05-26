90a63d71c57fb5456e736cd8e2ec0b47e1449ff4
charindun <nuwancharindu@gmail.com>
2024-05-26 02:17:51 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -25,7 +25,7 @@ global.mess = {
 global.owner = [process.env.OWNER_NUMBER] || ['94711453097'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
 
 module.exports = {    
-SESSION_ID: process.env.SESSION_ID || 'youre session id',
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=TdVBmCTZ#JpLa0V6YXAFAA5XCCMaUeGOjBtU-FBiKBtWy5k_4gBo',
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
 PREFIX:  process.env.PREFIX || ['.'] ,
 LANG:  process.env.LANG || 'SI' ,    
@@ -39,11 +39,11 @@ ALWAYS_ONLINE:  process.env.ALWAYS_ONLINE  || false  ,
 WORK_TYPE: process.env.WORK_TYPE || 'private'  ,
 ANTI_LINK: process.env.ANTI_LINK || false  ,
 ANTI_BOT: process.env.ANTI_BOT || false  ,
-OWNER_NUMBER: process.env.OWNER_NUMBER || '94719199757'  ,    
+OWNER_NUMBER: process.env.OWNER_NUMBER || '94762890901'  ,    
 ALIVE: process.env.ALIVE || `default`,
 OWNER_NAME: process.env.OWNER_NAME || 'Vajira' ,     
-AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || false  ,    
-AUTO_VOICE:  process.env.AUTO_VOICE  || false  ,
+AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || true  ,    
+AUTO_VOICE:  process.env.AUTO_VOICE  || true  ,
 AUTO_STICKER: process.env.AUTO_STICKER || false  ,
 WELCOME:  process.env.WELCOME  || false ,    
 NUMBER_212_BLOCK: process.env.NUMBER_212_BLOCK || false  ,
